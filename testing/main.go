b71ef96889ffa5b072b4a4fe64ffc29358703420
mrspec7er <wijayakusumasandi@gmail.com>
2023-08-11 18:07:53 +0800

    First Trial

---
+++ testing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main () {
+	fmt.Println("Hello There!");
+
+	const constantVariable = "This is how to define constant variable in GO!"
+
+	var mutateableVariable = "And this is the changeble variable"
+
+	mutateableVariable = "This variable can be change and redifined"
+
+	const totalTicket = 50
+
+	var ticketLeft = totalTicket - 7
+
+	fmt.Println(constantVariable, "And", mutateableVariable);
+
+	fmt.Println("Ticket Left", ticketLeft)
+}
